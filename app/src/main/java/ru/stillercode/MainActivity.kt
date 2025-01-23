f78f0ec8408f365599ce42c1a5edaa70216e8b1b
Stiller <schatirow@mail.ru>
2025-01-23 17:28:33 +0300

    bug with text change fixed somehow

--- app/src/main/java/ru/stillercode/MainActivity.kt
+++ app/src/main/java/ru/stillercode/MainActivity.kt
@@ -18,18 +18,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding = ActivityMainBinding.inflate(layoutInflater)
         buttonGrid = makeButtonGrid()
         updateButtons()
         println(buttonGrid[0][0])
-        binding.upperText.text = "Check"
+
     }
+
 
     fun boardTapped(view: View) {
         if (view !is Button) {
